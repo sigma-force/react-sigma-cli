afa682359093af151b0e24778293e368fdbe2a6b
Iván D. Sánchez <cosmosoft.soluciones@gmail.com>
2020-04-21 03:12:45 -0500

    First version of commmand react-cli for test

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 var shell = require('shelljs');
+//var path = require('path')
 
-const installReactCli = () => {
-  const path = shell.pwd();
-  shell.cd(path);
+function installReactCli() {
+  const path = shell.pwd().stdout;
+  shell.cd(path);  
   shell.exec('git clone https://github.com/sigma-force/react-base.git');
-};
+  shell.mv('-f',['./react-base/*','./react-base/.*'],'.');
+  shell.rm('-r','./react-base');
+}
+
+//installReactCli();
 
 module.exports = { installReactCli };
