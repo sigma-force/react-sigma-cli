be3cdc6cd7c93874497ab97584142e37408bc3ed
Andrés Santana <drew.lizcano@gmail.com>
2020-07-28 21:31:04 -0500

    CLI crearte comments

--- src/index.ts
+++ src/index.ts
@@ -1,46 +1,62 @@
 #!/usr/bin/env node
-import chalk from 'chalk';
-import clear from 'clear';
-import figlet from 'figlet';
-import inquirer from 'inquirer';
-import shell from 'shelljs';
+import chalk from "chalk";
+import clear from "clear";
+import figlet from "figlet";
+import inquirer from "inquirer";
+import shell from "shelljs";
 import defaultPackage from "./tools/package";
-import fs from 'fs';
+import fs from "fs";
+import path from 'path';
 
 clear();
 console.log(
-  chalk.blue(
-    figlet.textSync('React Sigma Cli', { horizontalLayout: 'full' })
-  )
+  chalk.blue(figlet.textSync("React Sigma Cli", { horizontalLayout: "full" }))
 );
 
 const run = async () => {
-  const prompt =  await inquirer.prompt([
+  const prompt = await inquirer.prompt([
     {
-      name: 'projectName',
-      type: 'input',
-      message: 'Enter your project name:',
-      validate: (value: any ) => {
+      name: "projectName",
+      type: "input",
+      message: "Enter your project name:",
+      validate: (value: any) => {
         if (value.length) {
           return true;
         } else {
-          return 'Enter your project name:';
+          return "Enter your project name:";
         }
-      }
+      },
+      default: path.basename(process.cwd())
+    },
+    {
+      name: "typeScript",
+      type: "confirm",
+      message: "Do yo want to use typeScript",
+      default: false
+    },
+    {
+      name: "framework",
+      type: "list",
+      message: "Pick the framework you are going to use:",
+      choices: ["Sass", "Stylus", "Css"]
     }
   ]);
-  defaultPackage.name = prompt.projectName
+  defaultPackage.name = prompt.projectName;
   const path = shell.pwd().stdout;
-  shell.cd(path);  
-  shell.exec(`git clone https://github.com/sigma-force/react-base.git ${prompt.projectName}`);
-  shell.cd(prompt.projectName);
-  shell.rm('-r','package.json');
-  shell.rm('-r','package-lock.json');
-  let data = JSON.stringify(defaultPackage, null, 2);
-  fs.writeFileSync('package.json', data)
-  shell.exec(`npm install`);
-  shell.rm('-r','.git');
-  shell.exec(`git init && git add . && git commit -m "initial commit"`);
-}
+  if (prompt.framework === "Sass") {
+    console.log("Is Sass");
+  }
+  console.log(prompt);
+  // shell.cd(path);
+  // shell.exec(`git clone https://github.com/sigma-force/react-base.git ${prompt.projectName}`);
+  // shell.cd(prompt.projectName);
+  // shell.rm('-r','package.json');
+  // shell.rm('-r','package-lock.json');
+  // let data = JSON.stringify(defaultPackage, null, 2);
+  // fs.writeFileSync('package.json', data)
+  // shell.exec(`npm install`);
+  // shell.rm('-r','.git');
+  // shell.exec(`git init && git add . && git commit -m "initial commit"`);
+};
 
 run();
